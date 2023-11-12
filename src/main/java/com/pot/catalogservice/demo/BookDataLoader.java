5380a26b885d50400d4ac9ae9b87d406a73bb3f1
apotapov <alexandrpotapov90@gmail.com>
2023-11-12 20:28:40 +0300

    fix test

--- src/main/java/com/pot/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/pot/catalogservice/demo/BookDataLoader.java
@@ -4,14 +4,15 @@ import com.pot.catalogservice.domain.Book;
 import com.pot.catalogservice.domain.BookRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-//@Profile("testdata")
-@ConditionalOnProperty(name = "polar.testdata.enabled", havingValue = "true")
+@Profile("testdata")
+//@ConditionalOnProperty(name = "polar.testdata.enabled", havingValue = "true")
 public class BookDataLoader {
 
     private final BookRepository bookRepository;
